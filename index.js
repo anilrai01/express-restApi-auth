56308afb14ca9b4052bf0e1c65bc5bc0ecd95371
anil.rai01 <anil.rai01041998@gmail.com>
2020-06-24 00:18:33 +0545

    Added new route to post and connected to local DB along with get and post data implementation

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 const postRoute = require("./routes/posts");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 //Middlewares
 app.use(express.json());
+
+//DB Connection
+mongoose.connect(process.env.DB_SERVER, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection
+  .once("open", () => console.log("Connected to Local MongoDB Successfully"))
+  .on("error", () => console.log("Error establishing Database"));
+
 //Route middleware
 app.use("/api/posts", postRoute);
-
 app.get("/", (req, res) => {
   res.send("I am at Home API");
 });
--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,32 @@
 const router = require("express").Router();
+const Post = require("../models/Posts");
+const { registerPostValidate } = require("../models/Validation");
 
-router.get("/", (req, res) => {
-  res.send("You are at post rest api");
+router.get("/", async (req, res) => {
+  try {
+    const allPost = await Post.find();
+    res.send(allPost);
+  } catch (err) {
+    res.send({ message: err });
+  }
+});
+
+router.get("/:id", (req, res) => {
+  res.send("You are at Post " + req.params.id);
+});
+
+router.post("/", async (req, res) => {
+  const post = new Post({
+    post_title: req.body.post_title,
+    post_desc: req.body.post_desc,
+  });
+
+  const { error } = registerPostValidate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  // const post = await post.save();
+  // res.send(post);
+  const newPost = await post.save();
+  res.send(newPost);
 });
 
 module.exports = router;
